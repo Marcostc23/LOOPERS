9ff3f558efc5877c0a276f8515e5cbbba09807eb
Enol Puente <epuente@campuscamarafp.com>
2026-01-20 09:58:04 +0100

    Fasefinal Despliegue vercel

--- backend/catalogo.php
+++ backend/catalogo.php
@@ -159,7 +159,7 @@ $resultado = $conexion->query($sql);
 <div class="container pb-5">
     <div class="header-section text-center">
         <div class="d-flex justify-content-between align-items-center mb-5">
-            <a href="index.php" class="text-secondary text-decoration-none small fw-bold" style="letter-spacing:2px">
+            <a href="../frontend/index.html" class="text-secondary text-decoration-none small fw-bold" style="letter-spacing:2px">
                 <i class="fas fa-long-arrow-alt-left me-2"></i> VOLVER
             </a>
             
