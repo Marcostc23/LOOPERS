8f719780743d8802c4acd534ec4f8651ddd1e360
Loopers studio <nuevoemail@example.com>
2026-01-27 10:40:14 +0100

    modifico conexion final

--- backend/conexion.php
+++ backend/conexion.php
@@ -3,10 +3,11 @@ $host = getenv('MYSQLHOST');
 $user = getenv('MYSQLUSER');
 $pass = getenv('MYSQLPASSWORD');
 $db   = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT');
 
 try {
     $conexion = new mysqli($host, $user, $pass, $db, $port);
     $conexion->set_charset(charset: "utf8mb4");
 } catch (Exception $e) {
     die("Error de conexión: " . $e->getMessage());
-}
+}   
